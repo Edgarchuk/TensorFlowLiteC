286f8464e9a02c3e71cc32f589e8ec9518450b39
Edgar Nazyrov <nazyrov01@gmail.com>
2022-03-11 16:01:23 +0500

    codesign

--- createXCFramework.sh
+++ createXCFramework.sh
@@ -1,3 +1,4 @@
 ./3rdparty/make-xcframework -ios ./loadTensorFlow/Pods/TensorFlowLiteC/Frameworks/TensorFlowLiteC.framework -arm64sim -output .
 ./3rdparty/make-xcframework -ios ./loadTensorFlow/Pods/TensorFlowLiteC/Frameworks/TensorFlowLiteCCoreML.framework -arm64sim -output .
 ./3rdparty/make-xcframework -ios ./loadTensorFlow/Pods/TensorFlowLiteC/Frameworks/TensorFlowLiteCMetal.framework -arm64sim -output .
+
